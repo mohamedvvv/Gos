06078ea1c8c3f5668cadea6f7995636454361201
mohamedvvv <57736339+mohamedvvv@users.noreply.github.com>
2022-12-22 18:07:56 +0200

    Update YOne.lua

--- YOne.lua
+++ YOne.lua
@@ -10,6 +10,21 @@ if not FileExist(COMMON_PATH .. "GGPrediction.lua") then
 end
 
 
+local function GetDistanceSqr1(pos1, pos2)
+	local pos2 = pos2 or myHero.pos
+	local dx = pos1.x - pos2.x
+	local dz = (pos1.z or pos1.y) - (pos2.z or pos2.y)
+	return dx * dx + dz * dz
+end
+
+local function GetDistance1(pos1, pos2)
+	return math.sqrt(GetDistanceSqr1(pos1, pos2))
+end
+
+
+
+
+
 local GameHeroCount     = Game.HeroCount
 local GameHero          = Game.Hero
 
@@ -22,11 +37,24 @@ local Enemys =   {}
 local Allys  =   {}
 local Units = 	 {}
 local myHero = myHero
-
+QSpellData = {speed = 1700, range = 900, delay = 0.35, radius = 120, collision = {nil}, type = "linear"}
+RSpellData = {speed = 1500, range = 1000, delay = 0.75, radius = 120, collision = {nil}, type = "linear"}	
+RspellData = {speed = 1500, range = 900, delay = 0.5, radius = 120, collision = {nil}, type = "linear"}
+R2spellData = {speed = 1500, range = 1200, delay = 0.75, radius = 120, collision = {nil}, type = "linear"}	
 function LoadUnits()
 	for i = 1, Game.HeroCount() do
 		local unit = Game.Hero(i); Units[i] = {unit = unit, spell = nil}
 	end
+end
+
+function CastingChecks()
+	local CastingQ = myHero.activeSpell.name == "YoneQ" or myHero.activeSpell.name == "YoneQ2" or myHero.activeSpell.name == "YoneQ3"
+	local CastingW = myHero.activeSpell.name == "YoneW"
+	local CastingE = myHero.activeSpell.name == "YoneE"
+	local CastingR = myHero.activeSpell.name == "YoneR"
+
+	local CastingChecksReturn = not CastingQ and not CastingW and not CastingE and not CastingR and not (myHero.pathing and myHero.pathing.isDashing) 
+	return CastingChecksReturn
 end
 
 local function GetDistanceSqr(pos1, pos2)
@@ -104,6 +132,8 @@ local function GetMinionCount(range, pos)
 	end
 	return count
 end
+
+
 
 local function GetEnemyCount(range, pos)
     local pos = pos.pos
@@ -221,6 +251,7 @@ function Yone:__init()
 	end	
 
     Callback.Add("Tick", function() self:Tick() end)
+	
 end
 
 
@@ -234,8 +265,14 @@ function Yone:LoadMenu()
 		self.tyMenu.combo:MenuElement({id = "useQL", name = "[Q1]/[Q2]", value = true})
         self.tyMenu.combo:MenuElement({id = "useQ3", name = "[Q3]", value = true})	
         self.tyMenu.combo:MenuElement({id = "useW", name = "[W]", value = true})
-	    self.tyMenu.combo:MenuElement({id = "useR", name = "[R] multible Enemies", value = true})
+		self.tyMenu.combo:MenuElement({id = "semir", name = "manual r key", key = string.byte("S")})
+		self.tyMenu.combo:MenuElement({id = "useE1", name = "[E1]", value = true})
+		
+	    self.tyMenu.combo:MenuElement({id = "useR", name = "[R] multiple Enemies", value = true})
 	    self.tyMenu.combo:MenuElement({id = "Count", name = "Min Enemies for [R]", value = 3, min = 2, max = 5})
+	    --self.tyMenu.combo:MenuElement({id = "qaoecount", name = "Min Enemies near to use [Q]aoe(will only cast when canhit target)", value = 3, min = 1, max = 5})
+		--self.tyMenu.combo:MenuElement({id = "enemiesforaoeq", name = "Min Enemies hit to use aoeq", value = 3, min = 2, max = 5})
+		
 		
     self.tyMenu:MenuElement({type = MENU, id = "harass", name = "Harass"})
         self.tyMenu.harass:MenuElement({id = "useQL", name = "[Q1]/[Q2]", value = true})
@@ -271,6 +308,11 @@ function Yone:Tick()
 	if Control.IsKeyDown(HK_Q) then
 		Control.KeyUp(HK_Q)
 	end
+	
+	qdelay=(0.4*(1-(0.598*(myHero.attackSpeed-1))))
+	if qdelay<0.133 then
+		qdelay=0.133
+	end
 
     local enemys = _G.SDK.ObjectManager:GetEnemyHeroes(1500)
 
@@ -280,9 +322,14 @@ function Yone:Tick()
     if myHero.dead or Game.IsChatOpen() or (_G.JustEvade and _G.JustEvade:Evading()) or (_G.ExtLibEvade and _G.ExtLibEvade.Evading) then
         return
     end
-
-    self:UpdateQDelay()
-
+	--print(self.Q.delay)
+   -- self:UpdateQDelay()
+	if self.tyMenu.combo.semir:Value() then
+		target = self:GetHeroTarget(1300)
+		if target and self.tyMenu.combo.semir:Value() then
+		self:CastRE(target)
+		end
+	end	
     if _G.SDK.Orbwalker.Modes[0] then --combo
 			self:combo()	
     elseif _G.SDK.Orbwalker.Modes[1] then --harass
@@ -293,55 +340,212 @@ function Yone:Tick()
     elseif _G.SDK.Orbwalker.Modes[4] then --lasthit
         self:LastHit()
     end
-    -- print(self.Q3.delay)
+    
 end
 
 
-function Yone:UpdateQDelay()
-    local activeSpell = myHero.activeSpell
+-- function Yone:UpdateQDelay()
+    -- local activeSpell = myHero.activeSpell
 
-    if activeSpell.valid then
-        if activeSpell.name == "YoneQ1" or activeSpell.name == "YoneQ2" then
-            self.Q.delay = activeSpell.windup
-        end
+    -- if activeSpell.valid then
+        -- if activeSpell.name == "YoneQ1" or activeSpell.name == "YoneQ2" then
+            -- self.Q.delay = activeSpell.windup
+			-- --print(self.Q.delay)
+        -- end
 
-        if activeSpell.name == "YoneQ3" then
-            self.Q3.delay = activeSpell.windup
+        -- if activeSpell.name == "YoneQ3" then
+            -- self.Q3.delay = activeSpell.windup
 
-            -- print(self.Q3.delay)
-        end
-    end
-end
+            -- -- print(self.Q3.delay)
+        -- end
+    -- end
+-- end
 
 
 function Yone:combo()
     local target = nil
+	local EnemyCount = GetEnemyCount(1300, myHero)
+	target = self:GetHeroTarget(1300)
 	
-	target = self:GetHeroTarget(self.R.range)
-    if target and self.tyMenu.combo.useR:Value() and self.tyMenu.combo.Count:Value() then
-	    local count = GetEnemyCount(1000, target)
-		if count >= self.tyMenu.combo.Count:Value() then
-        self:CastR(target)
-		end
+    if target and Ready(_R) and self.tyMenu.combo.useR:Value() and self.tyMenu.combo.Count:Value() and EnemyCount >= self.tyMenu.combo.Count:Value() and CastingChecks() then
+	    self:raoe(target,self.tyMenu.combo.Count:Value())
+		
     end
 	
 	target = self:GetHeroTarget(self.W.range)
-    if target and self.tyMenu.combo.useW:Value()then
+    if target and self.tyMenu.combo.useW:Value() and CastingChecks() then
         self:CastW(target)
     end
 	
     target = self:GetHeroTarget(self.Q3.range)
-    if target and self.tyMenu.combo.useQ3:Value()then
+
+     if target and Ready(_Q) and self.tyMenu.combo.useQ3:Value() and CastingChecks()  and myHero:GetSpellData(0).name == "YoneQ3"  then
         self:CastQ3(target)
-    end
+	end
+    -- if target and Ready(_Q) and self.tyMenu.combo.useQ3:Value() and CastingChecks() and (EnemyCount < self.tyMenu.combo.enemiesforaoeq:Value()) and myHero:GetSpellData(0).name == "YoneQ3"  then
+        -- self:CastQ3(target)
+	-- elseif target and Ready(_Q) and CastingChecks() and (EnemyCount >= self.tyMenu.combo.enemiesforaoeq:Value() ) and myHero:GetSpellData(0).name == "YoneQ3"  then
+		-- --self:qaoe(target,self.tyMenu.combo.qaoecount:Value())
+		-- self:CastQ3(target)
+    -- end
 
     target = self:GetHeroTarget(self.Q.range)
-    if self.tyMenu.combo.useQL:Value() then
+    if self.tyMenu.combo.useQL:Value() and CastingChecks() and myHero:GetSpellData(0).name  ~= "YoneQ3"   then
 		if target then
 			self:CastQ(target)	
 		end	
     end
 end
+  -- local pred = _G.PremiumPrediction:GetAOEPrediction(myHero, unit, RSpellData)
+  -- local predHit = _G.PremiumPrediction:GetHitChance(myHero, unit, pred.castPos, RSpellData, pred.timeToHit, pred.canHit) --returns hitChance to unit from pred
+  -- if pred.CastPos and predHit > 0 then 
+    -- if pred.HitCount >= hits then
+      -- Control.CastSpell(HK_R, pred.CastPos)
+    -- end
+  -- end
+-- end
+
+function Yone:raoe(unit,hits)
+
+	local RPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.8, Radius = 120, Range = 1000, Speed = 1500, Collision = false})
+	local minhitchance = self.tyMenu.Pred.PredR:Value()+1
+	local pred=RPrediction:GetAOEPrediction(myHero)
+	local bestaoe = nil
+	local bestcount = 0
+	local bestdistance = 1000
+	local containsUnit --maybe
+	for i = 1, #pred do
+		local aoe = pred[i]
+		if aoe.HitChance >= minhitchance and aoe.TimeToHit <= 2 and aoe.Count >= hits and aoe.Unit.networkID == unit.networkID then
+			if  aoe.Count > bestcount or (aoe.Count == bestcount and aoe.Distance < bestdistance) then
+				bestdistance = aoe.Distance
+				bestcount = aoe.Count
+				bestaoe = aoe
+	 
+				
+				end
+			end
+		end
+
+
+
+	if bestaoe  then
+		Control.CastSpell(HK_R, bestaoe.CastPosition)
+	end
+end
+
+
+
+function Yone:qaoe(unit,hits)
+
+	local QPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.35, Radius = 120, Range = 900, Speed = 1700, Collision = false})
+	local minhitchance = self.tyMenu.Pred.PredQ:Value()+1
+	local pred=QPrediction:GetAOEPrediction(myHero)
+	local bestaoe = nil
+	local bestcount = 0
+	local bestdistance = 900
+	
+	for i = 1, #pred do
+		local aoe = pred[i]
+		if aoe.HitChance >= minhitchance and aoe.TimeToHit <= 0.95 and aoe.Count >= hits and aoe.Unit.networkID == unit.networkID  then
+			if  aoe.Count > bestcount or (aoe.Count == bestcount1 and aoe.Distance < bestdistance) then
+				bestdistance = aoe.Distance
+				bestcount = aoe.Count
+				bestaoe = aoe
+	 
+				
+				end
+			end
+		end
+
+
+
+	if bestaoe then
+		Control.CastSpell(HK_Q, bestaoe.CastPosition)
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Yone:CastRE(unit)
+	--this is manual ult, will cast q3 instead if it's up
+	
+	--local E1Ready = self.tyMenu.combo.useE1:Value() and Ready(_E) and myHero.mana == 0
+	
+	
+	--if myHero.mana > 0 or not E1Ready then
+		
+		local RPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.5, Radius = 120, Range = 1000, Speed = 1500, Collision = false})
+		RPrediction:GetPrediction(unit, myHero)
+		if RPrediction:CanHit(self.tyMenu.Pred.PredR:Value()) and Ready(_R) then
+			Control.CastSpell(HK_R, RPrediction.CastPosition)
+		
+	--elseif E1Ready then 
+	--	Yone:CastE1Smart(unit)
+	end
+end	
+
+
+-- function Yone:CastE1Smart(unit)
+	
+	-- local E1Ready = Ready(_E) and myHero.mana == 0
+	
+	
+	-- --if E1Ready then
+		-- local EnemyCount1 = GetEnemyCount(600, unit)
+		-- if EnemyCount1 > 1 then
+			-- for i, Enemy in ipairs(GetEnemyHeroes()) do
+				-- if Enemy and GetDistance1(Enemy.pos, unit.pos) < 600 then
+	        		-- local EDirection = Vector((unit.pos - Enemy.pos):Normalized())
+	        		-- local EDistance = GetDistance1(unit.pos, Enemy.pos) + 300
+	        		-- local ESpot = unit.pos + EDirection * 300
+	        		-- if GetDistance1(ESpot) < 300 and GetDistance1(ESpot, unit.pos) < 600 then
+	        			-- Control.CastSpell(HK_E, ESpot)
+	        		-- end
+				-- end
+			-- end
+		-- else
+			-- Control.CastSpell(HK_E, unit)
+		-- end
+	-- end
+
+
+
+
+
+
+
+
+
+
+
+
 
 function Yone:harass()
     local target = nil
@@ -431,7 +635,7 @@ function Yone:LastHit()
 			end	
         end		
 
-        if self.tyMenu.last.useQL:Value() and Ready(_Q) and myHero.pos:DistanceTo(minion.pos) < 475 and myHero:GetSpellData(0).name ~= "YoneQ3" and _G.SDK.Orbwalker:CanMove(myHero) and self.lastQTick + 300 then
+        if self.tyMenu.last.useQL:Value() and Ready(_Q) and myHero.pos:DistanceTo(minion.pos) < 490 and  myHero.pos:DistanceTo(minion.pos) > 310 and CastingChecks() and myHero:GetSpellData(0).name ~= "YoneQ3" and _G.SDK.Orbwalker:CanMove(myHero) and self.lastQTick + 300 then
 			local QDmg = self:GetQDamge(minion)
 			if QDmg > minion.health then
 				Control.CastSpell(HK_Q, minion.pos)
@@ -475,7 +679,11 @@ end
 
 
 function Yone:CastW(target)
+	local CheckCount = GetEnemyCount(900, myHero)
+	local WPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_CONE, Delay = 0.15, Radius = 0, Range = 600, Angle = 80, Speed = MathHuge, Collision = false})
+	local CheckTargetCount = GetEnemyCount(250, target)
 	if Ready(_W) 
+	
 	and myHero:GetSpellData(0).name ~= "YoneQ3" 
 	and myHero.pos:DistanceTo(target.pos) <= self.W.range 
 	and _G.SDK.Orbwalker:CanMove(myHero) 
@@ -487,15 +695,129 @@ function Yone:CastW(target)
 				Control.CastSpell(HK_W, pred.CastPos)
 				self.lastQTick = GetTickCount()
 			end
+			
+			
+			
+			
+		
 		else
-			local WPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.15, Radius = 0, Range = 600, Speed = 500, Collision = false})
-				  WPrediction:GetPrediction(target, myHero)
-			if WPrediction:CanHit(self.tyMenu.Pred.PredW:Value()+1) then
-				Control.CastSpell(HK_W, WPrediction.CastPosition)
-				self.lastQTick = GetTickCount()
+				-- for i, Enemy in ipairs(GetEnemyHeroes()) do
+		-- if Enemy and myHero.pos:DistanceTo(Enemy.pos) <= 600 and IsValid(Enemy) then
+			if CheckCount >= 2 then 
+				Yone:CastWGGPred()
+			elseif myHero.pos:DistanceTo(target.pos) <= 500 and not Ready(_Q) then
+					Control.CastSpell(HK_W, target.pos)
 			end	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 		end
     end
+end
+
+
+function Yone:CastWGGPred(mintargets, maxtimetohit)
+    local WPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_CONE, Delay = 0.15, Radius = 0, Range = 600, Angle = 80, Speed = MathHuge, Collision = false})
+	local WPrediction1 = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.3, Radius = 300, Range = 600, Speed = 2000, Collision = false})
+	local minhitchance = 2
+    local aoeresult = WPrediction1:GetAOEPrediction(myHero)
+    local bestaoe = nil
+    local bestcount = 0
+    local bestdistance = 1000
+   
+	for i = 1, #aoeresult do
+        local aoe = aoeresult[i]
+        if aoe.HitChance >= minhitchance and aoe.TimeToHit <= 0.5 and aoe.Count >= 2 then
+            if aoe.Count > bestcount or (aoe.Count == bestcount and aoe.Distance < bestdistance) then
+                bestdistance = aoe.Distance
+                bestcount = aoe.Count
+                bestaoe = aoe
+            end
+        end
+    end
+    
+	if bestaoe then
+        Control.CastSpell(HK_W, bestaoe.CastPosition) 
+    end
+end
+
+
+
+
+local function HasBuffType(unit, type)
+    for i = 0, unit.buffCount do
+        local buff = unit:GetBuff(i)
+        if buff and buff.count > 0 and buff.type == type then
+            return true
+        end
+    end
+    return false
+
+end
+
+local function cantkill(unit,kill,ss,aa)
+	--set kill to true if you dont want to waste on undying/revive targets
+	--set ss to true if you dont want to cast on spellshield
+	--set aa to true if ability applies onhit (yone q, ez q etc)
+
+	for i = 0, unit.buffCount do
+	
+		local buff = unit:GetBuff(i)
+		if buff.name:lower():find("kayler") and buff.count==1 then
+			return true
+		end
+		
+	
+		if buff.name:lower():find("undyingrage") and (unit.health<100 or kill) and buff.count==1 then
+			return true
+		end
+		if buff.name:lower():find("kindredrnodeathbuff") and (kill or (unit.health / unit.maxHealth)<0.11) and buff.count==1  then
+			return true
+		end	
+		if buff.name:lower():find("chronoshift") and kill and buff.count==1 then
+			return true
+		end			
+		
+		if  buff.name:lower():find("willrevive") and kill and buff.count==1 then
+			return true
+		end
+		
+		
+		
+		if (buff.name:lower():find("fioraw") or buff.name:lower():find("pantheone")) and buff.count==1 then
+
+			return true
+		end
+		
+		if  buff.name:lower():find("jaxcounterstrike") and aa and buff.count==1  then
+			return true
+		end
+		
+		if  buff.name:lower():find("nilahw") and aa and buff.count==1  then
+			return true
+		end
+		
+		if  buff.name:lower():find("shenwbuff") and aa and buff.count==1  then
+			return true
+		end
+		
+	end
+	if HasBuffType(unit, 4) and ss then
+		return true
+	end
+
+	
+	
+	return false
 end
 
  
@@ -504,6 +826,8 @@ function Yone:CastQ(target)
 	and myHero:GetSpellData(0).name ~= "YoneQ3" 
 	and myHero.pos:DistanceTo(target.pos) <= self.Q.range 
 	and _G.SDK.Orbwalker:CanMove(myHero) 
+	and not cantkill(target,false,true,true)
+	
 	and self.lastQTick + 300 < GetTickCount() then
 		
 		if self.tyMenu.Pred.Change:Value() == 1 then
@@ -513,7 +837,8 @@ function Yone:CastQ(target)
 				self.lastQTick = GetTickCount()
 			end
 		else
-			local QPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.12, Radius = 15, Range = 500, Speed = 5000, Collision = false})
+			
+			local QPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = qdelay, Radius = 15, Range = 500, Speed = 5000, Collision = false})
 				  QPrediction:GetPrediction(target, myHero)
 			if QPrediction:CanHit(self.tyMenu.Pred.PredQ:Value()+1) then
 				Control.CastSpell(HK_Q, QPrediction.CastPosition)
@@ -527,8 +852,9 @@ function Yone:CastQ3(target)
     if Ready(_Q)
     and myHero:GetSpellData(0).name == "YoneQ3"  
     and myHero.pos:DistanceTo(target.pos) <= self.Q3.range 
-    and _G.SDK.Orbwalker:CanMove(myHero) 
-    and self.lastQTick + 300 < GetTickCount() then
+    and _G.SDK.Orbwalker:CanMove(myHero)
+    and self.lastQTick + 300 < GetTickCount() 
+	and not cantkill(target,false, true,true) then
 		
 		if self.tyMenu.Pred.Change:Value() == 1 then
 			local pred = _G.PremiumPrediction:GetPrediction(myHero, target, self.Q3)
@@ -537,7 +863,7 @@ function Yone:CastQ3(target)
 				self.lastQTick = GetTickCount()
 			end
 		else
-			local QPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.12, Radius = 50, Range = 1000, Speed = 500, Collision = false})
+			local QPrediction = GGPrediction:SpellPrediction({Type = GGPrediction.SPELLTYPE_LINE, Delay = qdelay, Radius = 80, Range = 1000, Speed = 1500, Collision = false})
 				  QPrediction:GetPrediction(target, myHero)
 			if QPrediction:CanHit(self.tyMenu.Pred.PredQ3:Value()+1) then
 				Control.CastSpell(HK_Q, QPrediction.CastPosition)
